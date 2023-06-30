cc39a8deb2551a0bc9837aa7b2c41aba56e61150
DPP-UNI <m.pelc.dpp@gmail.com>
2023-06-30 19:28:58 +0200

    XML output implementation

--- Makefile
+++ Makefile
@@ -11,3 +11,5 @@ run:
 	python main.py
 docker_build:
 	docker build -t hello-world-printer .
+test_smoke:
+	curl --fail 127.0.0.1:5000  
--- test/test_formater.py
+++ test/test_formater.py
@@ -1,4 +1,4 @@
-from hello_world.formater import plain_text_upper_case
+from hello_world.formater import plain_text_upper_case, format_to_xml
 import unittest
 
 
@@ -9,3 +9,6 @@ class TestFormater(unittest.TestCase):
         msg = r.split(" ")[1]
         self.assertTrue(name.isupper())
         self.assertTrue(msg.isupper())
+    def test_format_to_xml(self):
+        r = format_to_xml("wwww", "EEEMSG")
+        self.assertTrue(r == ("<greetings>\n\t<name>wwww</name>\n\t<msg>EEEMSG</msg>\n</greetings>"))
