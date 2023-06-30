eb98a9c44dc7e358b93c01fc99f278f3316bb856
DPP-UNI <m.pelc.dpp@gmail.com>
2023-06-30 19:52:56 +0200

    12

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,6 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Michal"
 msg = "Hello World!"
 
 @app.route('/')
@@ -11,7 +10,10 @@ def index():
     output = request.args.get('output')
     if not output:
         output = PLAIN
-    return get_formatted(msg, moje_imie,
+    name = request.args.get('name')
+    if not name:
+        name = "moje_imie"
+    return get_formatted(msg, name,
                          output.lower())
 
 @app.route('/outputs')
